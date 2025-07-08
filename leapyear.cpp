308e8391f28346aed8644cebb970ced242b7181f
Prince Mathur <princemathur1596@gmail.com>
2025-07-08 15:41:40 +0530

    Code to check if leap year or not

---
+++ leapyear.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cout << "Enter a year:";
+    cin >> year;
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        cout << "It is leap year";
+    } else {
+        cout << "It is not a leap year";
+    }
+    return 0;
+}
+
+
