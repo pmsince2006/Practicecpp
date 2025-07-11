aa669af32d872b167874ef022c7184a7ff724399
Prince Mathur <princemathur1596@gmail.com>
2025-07-11 13:51:25 +0530

    Nested loops

--- invertedright.cpp
+++ invertedright.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
     int n;
     cin >> n;
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=i;j++){
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
             cout << "* ";
         }
         cout << endl;
