4619fd8d3e6c78ed8deb639f3111b9d8cd890e4f
Prince Mathur <princemathur1596@gmail.com>
2025-08-04 14:22:00 +0530

    Modified structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -43,19 +43,19 @@
 // }
 
 
-#include<iostream>
-using namespace std;
-class Circle {
-public:
-    double radius;
-};
-int main() {
-    Circle* c = new Circle;
-    c->radius = 4.2;
-    cout << "Radius: "<<c->radius;
-    delete c;
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// class Circle {
+// public:
+//     double radius;
+// };
+// int main() {
+//     Circle* c = new Circle;
+//     c->radius = 4.2;
+//     cout << "Radius: "<<c->radius;
+//     delete c;
+//     return 0;
+// }
 
 // #include<iostream>
 // using namespace std;
